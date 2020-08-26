0d6b2cb04b6296de84ae7467de28339763653c45
nikikalwar <nikikumar27@gmail.com>
2020-08-26 05:34:47 +0530

    Day 4 problem completed

---
+++ 10daysJs/Obj1.js
@@ -0,0 +1,42 @@
+//
+
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.trim().split('\n').map(string => {
+        return string.trim();
+    });
+    
+    main();    
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/*
+ * Complete the Rectangle function
+ */
+function Rectangle(a, b) {
+    let obj1=new Object();
+    
+        obj1.length=a;
+       obj1.width=b;
+
+        obj1.perimeter=2*(a+b),
+        obj1.area=(a*b)
+
+   return obj1;
+
+}
+
